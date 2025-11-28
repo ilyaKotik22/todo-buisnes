90b68c128b2d00bb4736e61a1b0e241960afbbe7
ilyakotik22 <ilyakotik2204@gmail.com>
2025-11-28 11:37:03 +0300

    update config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,                  // важно!
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts', // ← ЭТО ГЛАВНОЕ!
+    css: true,
+  },
+})
